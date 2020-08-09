201d21849b2abe509ad47b1bc0fce1de818503d6
cat-ding <catherineding28@gmail.com>
2020-08-09 19:10:53 -0400

    user is able to pick a photo from gallery as profile photo

--- app/src/main/java/com/example/rumi/fragments/PhotoBottomSheetDialog.java
+++ app/src/main/java/com/example/rumi/fragments/PhotoBottomSheetDialog.java
@@ -1,10 +1,13 @@
 package com.example.rumi.fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -16,6 +19,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.example.rumi.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,11 +30,14 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class PhotoBottomSheetDialog extends BottomSheetDialogFragment {
 
     public static final String TAG = "PhotoBottomSheetDialog";
     private static final int CAPTURE_IMAGE_CODE = 35;
+    public static final int PICK_IMAGE_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
     private static final String KEY_FOLDER_NAME = "folderName";
     private static final String KEY_PHOTO_ID = "photoId";
 
@@ -83,6 +90,45 @@ public class PhotoBottomSheetDialog extends BottomSheetDialogFragment {
                 launchCamera();
             }
         });
+
+        tvChooseGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED) {
+                        // permission hasn't been granted so need to request
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        // permission already granted
+                        pickImageFromGallery();
+                    }
+                } else {
+                    // system os is less than marshmallow
+                    pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(getContext(), "Gallery access was denied!", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                }
+        }
     }
 
     private void launchCamera() {
@@ -102,6 +148,20 @@ public class PhotoBottomSheetDialog extends BottomSheetDialogFragment {
                 handleUpload(bitmap);
             } else {
                 Toast.makeText(getContext(), "Photo wasn't taken!", Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        }
+
+        if (requestCode == PICK_IMAGE_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+                    handleUpload(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getContext(), "No photo selected!", Toast.LENGTH_SHORT).show();
                 dismiss();
             }
         }
